c5e99314e9224efe099e8feda9c32b264d133d3d
Simone <startae14@gmail.com>
2021-09-21 16:40:19 +0200

    09: use GoogleMaps Type Definition Files to instantiate a new map inside the document

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import { User } from './User';
-import { Company } from './Company';
-const user = new User();
+// import { User } from './User';
+// import { Company } from './Company';
 
-console.log(user);
+// we need the reference to an html element to place the map
 
-const company = new Company();
-
-console.log(company);
-
-google
+new google.maps.Map(document.getElementById('map'), {
+  center: {
+    lat: 0,
+    lng: 0
+  },
+  zoom: 1
+});
